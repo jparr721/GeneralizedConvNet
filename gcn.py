1c379a3f8d2522aa6aa3d071995c1a4008bcf288
Jarred Parr <jparr721@gmail.com>
2019-01-19 10:40:10 -0500

    finished implementing

--- gcn.py
+++ gcn.py
@@ -40,8 +40,8 @@ class GeneralizedConvolutionalNetwork(object):
             name: str,
             kernel_size: tuple,
             n_output_channels: int,
-            padding_mode: str ='SAME',
-            strides: tuple =(1, 1, 1, 1)):
+            padding_mode: str = 'SAME',
+            strides: tuple = (1, 1, 1, 1)):
         '''
         The convolutional layer constructs a tensorflow convolution
         to facilitate reduction of code duplication. This module can
@@ -95,6 +95,14 @@ class GeneralizedConvolutionalNetwork(object):
                                 filter=weights,
                                 strides=strides,
                                 padding=padding_mode)
+
+            conv = tf.nn.bias_add(conv, biases,
+                                  name='net_pre_activation')
+
+            # Apply our activation function to our network
+            conv = tf.nn.relu(conv, name='activation')
+
+            return conv
 
     def fully_connected_layer(
             self,
@@ -151,15 +159,15 @@ class GeneralizedConvolutionalNetwork(object):
         '''
         This function creates the full convolutional network by combining
         several convolutional layers with a pooling layer. The pooling layer
-        reduces the dimensionality of the data at each convolution to facilitate
-        greater computational performance, and less needed system resources. We then
-        tie to a final fully connected multilayer perceptron to perform our final
-        prediction tasks
+        reduces the dimensionality of the data at each convolution to
+        facilitate greater computational performance, and less needed
+        system resources. We then tie to a final fully connected multilayer
+        perceptron to perform our final prediction tasks
 
         Parameters
         ----------
-        learning_rate: float - The learning rate for how big of jumps we want to
-                               make when running our optimizer
+        learning_rate: float - The learning rate for how big of jumps we want
+                               to make when running our optimizer
         '''
         # NOTE: The shape of this WILL CHANGE when we get the image dimens.
         tf_x = tf.placeholder(tf.float32, shape=[None, 784], name='tf_x')
@@ -216,16 +224,17 @@ class GeneralizedConvolutionalNetwork(object):
 
         # Make our predictions
         predictions = {
-            # Use a softmax axcitvation function on our output layer to get our probs
+            # Use a softmax axcitvation function on
+            # our output layer to get our probs
             'probabilities': tf.nn.softmax(layer_4, name='probabilities'),
-            'labels': tf.cast(tf.argmax(layer_4, axis=1), tf.in32, name='labels')
+            'labels': tf.cast(tf.argmax(
+                layer_4, axis=1), tf.in32, name='labels')
         }
-
 
         # Loss function and optimization and add it to our graph
         cross_entropy_loss = tf.reduce_mean(
                 tf.nn.softmax_cross_entropy_with_logits(
-                    logits=h4, labels=tf_y_onehot),
+                    logits=layer_4, labels=tf_y_onehot),
                 name='cross_entropy_loss')
 
         # Our optimizer function
@@ -233,7 +242,7 @@ class GeneralizedConvolutionalNetwork(object):
         optimizer = tf.train.AdamOptimizer(learning_rate)
         optimizer = optimizer.minimize(cross_entropy_loss, name='train_op')
 
-         # Computing the prediction accuracy
+        # Computing the prediction accuracy
         correct_predictions = tf.equal(
             predictions['labels'],
             tf_y, name='correct_preds')
@@ -241,3 +250,61 @@ class GeneralizedConvolutionalNetwork(object):
         accuracy = tf.reduce_mean(
             tf.cast(correct_predictions, tf.float32),
             name='accuracy')
+
+    def save_graph(self, saver, sess, epoch, path='./model'):
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        print('Saving model{}'.format(path))
+        saver.save(sess, os.path.join(
+            path, 'cnn-model.ckpt'), glboal_step=epoch)
+
+    def load(self, saver, sess, path, epoch):
+        print('Loading model from: {}'.format(path))
+        saver.restore(sess, os.path.join(
+            path, 'cnn-model.ckpt-{}'.format(epoch)))
+
+    def train(self, sess, training_set, validation_set=None,
+              initialize=True, epochs=20, shuffle=True,
+              dropout=0.5, random_seed=None):
+        X_data = np.array(training_set[0])
+        y_data = np.array(training_set[1])
+        training_loss = []
+
+        if initialize:
+            sess.run(tf.global_variables_initializer())
+
+        np.random.seed(random_seed)
+
+        for epoch in range(1, epochs + 1):
+            batch_gen = self.batch_generator(X_data, y_data, shuffle=shuffle)
+            avg_loss = 0.0
+
+            for i, (batch_x, batch_y) in enumerate(batch_gen):
+                feed = {'tf_x:0': batch_x,
+                        'tf_y:0': batch_y,
+                        'fc_keep_prob:0': dropout}
+                loss, _ = sess.run(
+                        ['cross_entropy_loss:0', 'train_op'],
+                        feed_dict=feed)
+                avg_loss += loss
+
+            training_loss.append(avg_loss / (i + 1))
+            print('Epoch {} Training Avg. Loss: {}'
+                  .format(epoch, avg_loss), end=' ')
+
+            if validation_set is not None:
+                feed = {'tf_x:0': validation_set[0],
+                        'tf_y:0': validation_set[1],
+                        'fc_keep_prob:0': 1.0}
+                valid_acc = sess.run('accuracy:0', feed_dict=feed)
+                print(' Validation Acc: {}'.format(valid_acc))
+            else:
+                print()
+
+    def predict(self, sess, X_test, return_probability=False):
+        feed = {'tf_x:0': X_test,
+                'fc_keep_prob:0': 1.0}
+        if return_probability:
+            return sess.run('probabilities:0', feed_dict=feed)
+        else:
+            return sess.run('labels:0', feed_dict=feed)
